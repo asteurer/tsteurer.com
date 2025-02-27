0a28c9b043bf0f1e44c68fe3add9edd3dae2ff19
Andrew Steurer <94206073+asteurer@users.noreply.github.com>
2025-02-27 00:50:05 +0000

    fix: update resume page
    Signed-off-by: Andrew Steurer <94206073+asteurer@users.noreply.github.com>

--- front_end/src/routes/resume/+page.svelte
+++ front_end/src/routes/resume/+page.svelte
@@ -1,19 +1,24 @@
 <script lang="ts">
-  const pdfUrl: string = 'https://s3.us-west-2.amazonaws.com/tsteurer.com/resume/resume.pdf';
+  import SplitBackground from '$lib/components/Background.svelte';
+  const resumeURL: string = 'https://s3.us-west-2.amazonaws.com/tsteurer.com/resume/resume.jpeg';
 </script>
-
-<div class="pdf-container">
-  <embed
-    src={pdfUrl}
-    type="application/pdf"
-    width="100%"
-    height="100%"
-  />
-</div>
+<SplitBackground>
+  <div class="resume-container">
+    <img src={resumeURL} alt="resume" class="resume">
+  </div>
+</SplitBackground>
 
 <style>
-  .pdf-container {
+  .resume-container {
     width: 100%;
     height: 90vh;  /* Takes up 90% of the viewport height */
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 0 auto;
+  }
+  .resume-container img {
+    border: 1px solid black;
+    max-width: 90%;
   }
 </style>
